3056d8666d1154813e9aa08543a4d3acc992d3c0
Lenmor Larroza Dimanalata <lenmorld@gmail.com>
2022-01-09 15:48:11 -0500

    UI 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {FiThumbsUp, FiThumbsDown} from 'react-icons/fi'
+
+import { movie, favorites } from './mocks'
+
 function App() {
+  const onThumbsDown = () => {
+    // - mark as unliked by this user
+    // don't show again
+    // next movie
+  }
+
+  const onThumbsUp = () => {
+    // add to favorites
+    // - mark as liked by this user
+    // - if also liked by other user > it's a match!
+
+    // add to matches
+    // next movie
+  }
+
+
+  const nextMovie = () => {
+    // get next movie
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        WHAT TO WATCH?
       </header>
+      <main>
+      <div className="favorites">
+        <h3>Favorites</h3>
+        {
+          favorites.length && (
+            <ul>
+              {
+                  favorites.map(fave => (
+                    <li>
+                      <div>
+                        <img src={fave.Poster} alt={fave.Title}  />
+                      </div>
+                    </li>
+                  ))
+              }
+            </ul>
+          )
+        }
+      </div>
+      <div className="wanna">
+        <h3>Wanna watch this?</h3>
+      </div>
+      <h2>{movie.Title}</h2>  
+      <div className="movie">
+        <div className="image">
+          <img src={movie.Poster} alt={movie.Title} />
+          <button className="nope" onClick={onThumbsDown}>
+            <FiThumbsDown />
+          </button>
+          <button className="yup" onClick={onThumbsUp}>
+            <FiThumbsUp />
+          </button>
+        </div>
+        <div className="description">
+          <ul>
+            <li><strong>Plot:</strong> {movie.Plot}</li>
+            <br />
+            <li>
+              <strong>Rating:</strong>{' '}
+              {
+                movie.Ratings.map(rating => (
+                  <span>{rating.Value}{' '}</span>
+                ))
+              }
+            </li>
+          </ul>
+        </div>
+      </div>
+      </main>
     </div>
   );
 }
---
+++ src/mocks.js
@@ -0,0 +1,78 @@
+export const movie = {
+    "Title": "Spider-Man: No Way Home",
+    "Year": "2021",
+    "Rated": "N/A",
+    "Released": "17 Dec 2021",
+    "Runtime": "N/A",
+    "Genre": "Action, Adventure, Sci-Fi",
+    "Director": "Jon Watts",
+    "Writer": "Chris McKenna, Erik Sommers, Stan Lee",
+    "Actors": "Zendaya, Benedict Cumberbatch, Tom Holland",
+    "Plot": "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.",
+    "Language": "English",
+    "Country": "United States, Iceland",
+    "Awards": "N/A",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMDUzNWJhZWQtYzU3Zi00M2NjLThjZjEtMTRmMjRmNzBmMWI2XkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_SX300.jpg",
+    "Ratings": [
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "95%"
+        }
+    ],
+    "Metascore": "N/A",
+    "imdbRating": "N/A",
+    "imdbVotes": "N/A",
+    "imdbID": "tt10872600",
+    "Type": "movie",
+    "DVD": "N/A",
+    "BoxOffice": "N/A",
+    "Production": "N/A",
+    "Website": "N/A",
+    "Response": "True"
+}
+
+export const movie2 = {
+    "Title": "King Richard",
+    "Year": "2021",
+    "Rated": "PG-13",
+    "Released": "19 Nov 2021",
+    "Runtime": "144 min",
+    "Genre": "Biography, Drama, Sport",
+    "Director": "Reinaldo Marcus Green",
+    "Writer": "Zach Baylin",
+    "Actors": "Will Smith, Aunjanue Ellis, Jon Bernthal",
+    "Plot": "A look at how tennis superstars Venus and Serena Williams became who they are after the coaching from their father Richard Williams.",
+    "Language": "English",
+    "Country": "United States",
+    "Awards": "5 wins & 2 nominations",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYTcyNmY4ZGEtYmE4Zi00ZDViLTlmYzMtMmQ4ZTM4OWNmZjQxXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+    "Ratings": [
+        {
+            "Source": "Internet Movie Database",
+            "Value": "6.3/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "91%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "71/100"
+        }
+    ],
+    "Metascore": "71",
+    "imdbRating": "6.3",
+    "imdbVotes": "11,830",
+    "imdbID": "tt9620288",
+    "Type": "movie",
+    "DVD": "19 Nov 2021",
+    "BoxOffice": "$6,652,531",
+    "Production": "N/A",
+    "Website": "N/A",
+    "Response": "True"
+}
+
+export const favorites = [
+    movie,
+    movie2
+]
